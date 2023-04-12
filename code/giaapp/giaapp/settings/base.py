71b80652c1c42ed159ca20876f35e8d15c78c0f9
freve <fredyhumbertovera@ufps.edu.co>
2023-04-12 00:56:08 -0500

    Ajustes para quitar apache del docker

--- code/giaapp/giaapp/settings/base.py
+++ code/giaapp/giaapp/settings/base.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','.localhost','gia.ufps.edu.co', '127.0.0.1:8000']
+ALLOWED_HOSTS = ['0.0.0.0','localhost','gia.ufps.edu.co', '127.0.0.1']
 DOMINIO = '.gia.ufps.edu.co'
 
 
@@ -126,7 +126,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [    
+    'giaapp/static/',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- code/giaapp/giaapp/settings/production.py
+++ code/giaapp/giaapp/settings/production.py
@@ -1,4 +1,5 @@
 from .base import *
 
-DEBUG = False
+DEBUG = True
 
+INTERNAL_IPS = ('127.0.0.1','0.0.0.0')
